7bb684f40fe055df622b0a5ef7f9fb10fa0af1d7
Raúl Ernesto Almao Alvarado <133242982+Ralmao@users.noreply.github.com>
2023-12-28 20:54:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+print("hello world")
+print("what is your name")
+myName =input()
+print("nice to meet you " + myName)
+print("the lenght of your name is ")
+print(len(myName))
+print("whats is your age ")
+myAge =input()
+print (" u will be " + str(int(myAge) + "1" ) + " in next year" )

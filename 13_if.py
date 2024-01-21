167d3c6d4b5f8fb2eaaea4e2fae82676eca937f8
Raúl Ernesto Almao Alvarado <133242982+Ralmao@users.noreply.github.com>
2024-01-20 21:53:44 -0400

    Add files via upload

---
+++ 13_if.py
@@ -0,0 +1,29 @@
+if True:
+  print("Deberia ejecutarse")
+
+if False:
+ print("nunca se ejecuta")
+
+
+pet = input("Cual es tu mascota favorita? ")
+if pet == "perro":
+  print("genial tienes buen gusto")
+
+elif pet == "gato":
+  print("espero tengas suerte")
+
+elif pet == "pez":
+  print("eres lo maximo")
+else :
+  print("no tienes ninguna mascota intersante")
+
+'''
+stock= input("Digita el stock =>")
+stock= int(stock)
+
+if stock >= 100 and stock <= 1000:
+  print("el stock es correcto")
+else:
+  print("el stock es incorrecto")
+'''
+
